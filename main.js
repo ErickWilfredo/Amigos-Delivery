b714d4ab9517238c01ed70c92ef039f9b336ecc2
AmigosDelivery <88524939+ErickWilfredo@users.noreply.github.com>
2021-08-05 23:35:19 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const enlaces = document.getElementsByClassName("enlaces")[0];
+const hamburguesa = document.getElementsByClassName("hamburguesa")[0];
+const menuHamburguesa = document.getElementsByClassName("hamburguesa");
+let abierto = false;
+
+const toggleMenu = () =>{
+    enlaces.classList.toggle("enlaces2");
+    enlaces.style.transition = "transform 0.5s ease-in-out"
+}
+
+
+hamburguesa.addEventListener("click", function(){
+    toggleMenu();
+    if(document.querySelector(".enlaces.enlace2")){
+       abierto=true;
+    }else{
+       abierto=false;
+    }
+})
+
+window.addEventListener("click", function(e){
+    this.console.log(e.target)
+    if(abierto){
+        if(e.target !== menuHamburguesa){
+            toggleMenu();
+            abierto=false;
+        }
+    }
+})
+
